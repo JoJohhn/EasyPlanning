f9f53022f7cae4201bd73c996f3d63a88eddfa8e
ToughMelomaniac <114314923+ToughMelomaniac@users.noreply.github.com>
2023-02-17 01:42:02 +0300

    added model Task

--- plannerapp/models.py
+++ plannerapp/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 
 class Task(models.Model):
-    taskName = models.CharField(max_length=255)
-    task = models.CharField(max_length=255)
+    taskName = models.CharField(max_length=255, null=False)
+    task = models.TextField(max_length=255, blank=True)
+    taskParent = models.IntegerField(null=False, default=-1)
+    userId = models.IntegerField(null=False, default=1)
+    taskStartTime = models.DateTimeField(auto_now=False, auto_now_add=False, null=False, default=timezone.now)
