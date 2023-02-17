943dab1f9b8f4c4b1898d31f9a0b403d6184492b
ToughMelomaniac <114314923+ToughMelomaniac@users.noreply.github.com>
2023-02-17 20:34:16 +0300

    added user id

--- plannerapp/models.py
+++ plannerapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,6 +8,10 @@ from django.utils import timezone
 class Task(models.Model):
     taskName = models.CharField(max_length=255, null=False)
     task = models.TextField(max_length=255, blank=True)
-    taskParent = models.IntegerField(null=False, default=-1)
-    userId = models.IntegerField(null=False, default=1)
-    taskStartTime = models.DateTimeField(auto_now=False, auto_now_add=False, null=False, default=timezone.now)
+    taskParent = models.IntegerField(null=False)
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    taskStartTime = models.DateTimeField(auto_now=False, auto_now_add=False, null=False)
+
+
+    def __str__(self):
+        return f'{self.taskName}'
