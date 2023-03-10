d066ff0df8610d9e3f60f859bc95c8ba3ad9482d
JoJohhn <vanka334420@gmail.com>
2023-03-10 16:45:17 +0300

    fixed regform

--- users/forms.py
+++ users/forms.py
@@ -10,10 +10,14 @@ class MyUserCreationForm(UserCreationForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
     password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    tgUsername = forms.ModelChoiceField(label='Телеграм', widget=forms.TextInput(attrs={'class': 'form-input'}), queryset=TgUserIds.objects.all(), to_field_name='name')
-    class Meta():
-        model = User
-        fields = {'username', 'password1', 'password2', 'tgUsername'}
+    tgUsername = forms.ModelChoiceField(label='Имя пользователя телеграм',
+                                        widget=forms.TextInput(attrs={'class': 'form-input'}),
+                                        queryset=TgUserIds.objects.all(),
+                                        to_field_name='name')
+    field_order = ['username', 'tgUsername', 'password1', 'password2']
+    # class Meta():
+    #     model = User
+    #     fields = {'username', 'password1', 'password2', 'tgUsername'}
         # fields = UserCreationForm.Meta.fields
         # widgets = {
         #     'username': forms.TextInput(attrs={'class': 'form-input'}),
